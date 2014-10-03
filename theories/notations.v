8f45543c4a56304d829d36e3c104dbd18f0f9004
tabareau <tabareau@dhcp-2-121.rech172-28.emn.fr>
2014-10-03 15:46:03 +0200

    adding tex (final)

---
+++ theories/notations.v
@@ -0,0 +1,26 @@
+(** printing ~1 $\sim_1$ *)
+(** printing ~2 $\sim_2$ *)
+(** printing Π1 $\pi_1$*)
+(** printing Π2 $\pi_2$*)
+(** printing --> $\longrightarrow$*)
+(** printing ---> $\longrightarrow$*)
+(** printing β $\beta$*)
+(** printing [! $\llbracket$*)
+(** printing !] $\rrbracket$*)
+(** printing |- $\vdash$*)
+(** printing === $\equiv$*)
+(** We use the following notations throughout: Sigma type introduction
+  is written [(t ; p)] when its predicate/fibration is inferrable from
+  the context, and projections are denoted [Π1] and [Π2]. The bracket
+  notation [[_]] is an alias for [Π1]. The following is directly
+  extracted from Coq .v files using the <<coqdoc>> tool (source files
+  are available at %\url{https://github.com/mattam82/groupoid}%). If
+  you are reading the colored version of the paper, keywords are
+  typeset in red, inductive types and classes in blue, inductive
+  constructors in dark red, and defined constants and lemmas in green.
+  *)
+(* begin hide *)
+Notation Π1 := projT1.
+Notation Π2 := projT2.
+Notation " ( x ; p ) " := (existT _ x p).
+(* end hide *)
